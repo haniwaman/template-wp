d8cfc28fec6a9c92682cca9e4527b042215399e8
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-03-21 02:30:33 +0900

    Update tags.php

--- inc/tags.php
+++ inc/tags.php
@@ -34,3 +34,35 @@ function my_the_post_category() {
 		echo '<a href="' . esc_url( $this_categories[0]['link'] ) . '">' . esc_html( $this_categories[0]['name'] ) . '</a>';
 	}
 }
+
+
+/**
+ * ターム取得
+ *
+ * @param string $taxonomy タクソノミーのスラッグ名.
+ * @return array ターム情報.
+ */
+function my_get_post_terms( $taxonomy ) {
+	$this_terms = array();
+	$terms      = get_the_terms( $post->ID, $taxonomy );
+	$term_num   = count( $terms );
+	for ( $i = 0; $i < $term_num; $i++ ) {
+		$this_terms[ $i ]['id']   = $terms[ $i ]->term_id;
+		$this_terms[ $i ]['name'] = $terms[ $i ]->name;
+		$this_terms[ $i ]['slug'] = $terms[ $i ]->slug;
+		$this_terms[ $i ]['link'] = get_term_link( $terms[ $i ]->term_id, $taxonomy );
+	}
+	return $this_terms;
+}
+
+/**
+ * タームを1つだけ表示
+ *
+ * @param string $taxonomy タクソノミーのスラッグ名.
+ */
+function my_the_post_term( $taxonomy ) {
+	$this_terms = my_get_post_terms( $taxonomy );
+	if ( isset( $this_terms[0] ) ) {
+		echo '<div class="shopnews-tag m_' . $this_terms[0]['slug'] . '">' . esc_html( $this_terms[0]['name'] ) . '</div>';
+	}
+}
