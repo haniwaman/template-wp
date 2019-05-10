f9baf4c425c3599d85143c6eb348688984a8cd9b
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-05-10 18:31:42 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /* drawer */
-jQuery( '.drawer-open' ).on( 'click', function() {
+jQuery( '.drawer-open, .drawer-close' ).on( 'click', function() {
 	jQuery( '.drawer-open' ).toggleClass( 'm_checked' );
 	jQuery( '.drawer-close' ).toggleClass( 'm_checked' );
 	jQuery( '.drawer-content' ).toggleClass( 'm_checked' );
