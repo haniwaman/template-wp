6c2ffc4ea7d611756e14863dbbceddf32752e0fa
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-05-26 21:20:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ get_header(); ?>
 						if ( has_post_thumbnail() ) {
 							the_post_thumbnail( 'my_thumbnail' );
 						} else {
-							echo '<img src="' . esc_url( get_template_directory_uri() ) . '/img/noimg.png" />';
+							echo '<img src="' . esc_url( get_template_directory_uri() ) . '/img/noimg.png" alt="">';
 						}
 						?>
 					</a>
