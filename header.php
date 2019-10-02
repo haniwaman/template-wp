7510d2a8112b7a663ebf1cf7a0bb0c6f1d0ffcca
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-10-02 20:51:25 +0900

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="format-detection" content="telephone=no">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <?php wp_head(); ?>
 </head>
 
