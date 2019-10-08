2c4f436f9d7cb5f98696637baff507f20bdb9f87
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-10-08 21:13:19 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,55 @@
-/* drawer */
-jQuery( '.drawer-open, .drawer-close' ).on( 'click', function() {
-	jQuery( '.drawer-open' ).toggleClass( 'm_checked' );
-	jQuery( '.drawer-close' ).toggleClass( 'm_checked' );
-	jQuery( '.drawer-content' ).toggleClass( 'm_checked' );
+// ローディング判定
+jQuery(window).on("load", function() {
+	jQuery("body").attr("data-loading", "true");
+});
+
+jQuery(function() {
+	// スクロール判定
+	jQuery(window).on("scroll", function() {
+		if (100 < jQuery(this).scrollTop()) {
+			jQuery("body").attr("data-scroll", "true");
+		} else {
+			jQuery("body").attr("data-scroll", "false");
+		}
+	});
+
+	/* ドロワー */
+	jQuery(".js-drawer").on("click", function(e) {
+		e.preventDefault();
+		let targetClass = jQuery(this).attr("data-target");
+		jQuery("." + targetClass).toggleClass("is-checked");
+		return false;
+	});
+
+	/* スムーススクロール */
+	jQuery('a[href^="#"]').click(function() {
+		let header = jQuery("#header").height();
+		let speed = 300;
+		let id = jQuery(this).attr("href");
+		let target = jQuery("#" == id ? "html" : id);
+		let position = jQuery(target).offset().top - header;
+		if ("fixed" !== jQuery("#header").css("position")) {
+			position = jQuery(target).offset().top;
+		}
+		if (0 > position) {
+			position = 0;
+		}
+		jQuery("html, body").animate(
+			{
+				scrollTop: position
+			},
+			speed
+		);
+		return false;
+	});
+
+	/* 電話リンク */
+	let ua = navigator.userAgent;
+	if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
+		jQuery('a[href^="tel:"]')
+			.css("cursor", "default")
+			.on("click", function(e) {
+				e.preventDefault();
+			});
+	}
 });
