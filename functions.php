f663d3e809b5c54f09582f03dad00b34720c57d7
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-10-15 21:29:07 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -37,6 +37,9 @@ function my_script_init() {
 	wp_enqueue_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all' );
 	wp_enqueue_style( 'my', get_template_directory_uri() . '/css/style.css', array(), '1.0.1', 'all' );
 	wp_enqueue_style( 'df', get_stylesheet_uri(), array(), array(), '1.0.1', 'all' );
+	
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), '3.3.1', true );
 	wp_enqueue_script( 'my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.1', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_script_init' );
